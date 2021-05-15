014ff20877dd5380b87d6210584623c9da5428ee
bbbnam <bbbnam@naver.com>
2021-05-16 04:56:30 +0900

    test : 로또번호 유효성 검사 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,10 +2,13 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
 
@@ -29,5 +32,16 @@ class LottoTest {
 
         assertThat(lotto.getNumbers()).hasSize(6);
         assertThat(numbers.get(0)).isEqualTo(new LottoNumber(1));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5", "1,2,3", "1,2,3,4,5,5"})
+    @DisplayName("로또 번호 유효성 검사")
+    void validateLottoNumbers(String numbers) {
+        Manual manual = new Manual(numbers);
+
+        assertThatThrownBy(() -> new Lotto(manual))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자값은 6개의 중복되지 않은 값만 선택 가능합니다.");
     }
 }
