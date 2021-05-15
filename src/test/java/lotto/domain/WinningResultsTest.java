e145c8fd766d34c785b4f87c555f14db2674d75d
bbbnam <bbbnam@naver.com>
2021-05-16 05:39:40 +0900

    test : 수익률 계산 테스트

---
+++ src/test/java/lotto/domain/WinningResultsTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinningResultsTest {
+
+    @Test
+    @DisplayName("수익률 계산 테스트")
+    void calculateProfitRate() {
+        long inputPrice = 14000L;
+        Map<WinningType, Integer> winnings = new HashMap<>();
+        winnings.put(WinningType.FOURTH, 1);
+
+        WinningResults winningResults = new WinningResults(winnings);
+
+        assertThat(winningResults.calculateProfitRate(inputPrice)).isEqualTo(0.35);
+    }
+}
